cfb061728f46b28e5117e5d4172b47ed1d496d87
badra <abnreddy.a@gmail.com>
2022-02-26 10:55:32 +0530

    Added templates with dynamic content

--- django_demo/contact/urls.py
+++ django_demo/contact/urls.py
@@ -1,9 +1,9 @@
 # we are going keep all contact app specific url here and map appropriate view function
 from django.urls import path
-from .views import index, get_contact, create_contact
+from .views import index, get_contact, create_contact, list_contacts
 
 urlpatterns = [
-    path('index', index),
-    path('get-contacts', get_contact),
-    path('create', create_contact),
+    path('index', index, name='index'),
+    path('get-contacts', get_contact, name='get-contacts'),
+    path('create', create_contact, name='create-contact'),
 ]
--- django_demo/contact/views.py
+++ django_demo/contact/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Contact
 
 # Create your views here.
 
@@ -10,17 +11,45 @@ def index(request):
 def get_contact(request):
     # we connect to db and fetch the contact data
     # and give in response to the user
-    data = {"Name": "Ravi", "age": 23}
-    return HttpResponse(data)
+    contacts = Contact.objects.all()
+
+    context = {'contacts': contacts}
+    return render(request, template_name='contact/contacts_list.html', context=context)
+
 
 
 def create_contact(request):
     if request.method == 'GET':
         return render(request, template_name='contact/create_contact.html')
     elif request.method == 'POST':
-        # read data from request oject
-        # store into db
-        pass
+        """
+        (Pdb) request.POST
+<QueryDict: {'csrfmiddlewaretoken': ['OYQVB64uuZco7sXREEf48Z7Uu7cXjKBCNADM3IM8zEVwSDzv5POg9CfKoWre2ytU'], 'name': ['ram'], 'mnumber': ['
+889999993'], 'dob': ['2022-02-18'], 'email': ['abnra@gmail.com'], 'location': ['hyderabad']}>
+        
+        """
+        # capturing user entered data on UI
+        name = request.POST['name']
+        mobile_number = request.POST['mnumber']
+        date_of_birth = request.POST['dob']
+        email = request.POST['email']
+        location = request.POST['location']
+
+        contact_obj = Contact(name=name, mobile_number=mobile_number,
+                              date_of_birth=date_of_birth, email=email, location=location)
+
+        # save the data into db
+        contact_obj.save()
+
+        #print(name, mobile_number, date_of_birth, email, location)
+
+        return redirect(to='get-contacts')
+
+def list_contacts(request):
+    # we will fecth the all record from contact table and render into html document
+    return HttpResponse("HDHDHDHHDHD")
+
+
 
 
 
